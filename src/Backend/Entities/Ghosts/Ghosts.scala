583a121dbce7ac70c75e841aaf4a898281c18d98
Axel Schneider <axel.schneider2003@gmail.com>
2025-12-31 10:49:32 +0100

    Enhance entity and ghost logic: add revive and kill methods, improve vulnerability handling, and refactor position correction methods

--- src/Backend/Entities/Ghosts/Ghosts.scala
+++ src/Backend/Entities/Ghosts/Ghosts.scala
@@ -6,41 +6,46 @@ import Backend.Entities.{Directions, Entity}
 import Backend.Logical
 
 import java.awt.Color
-import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
+import java.util.concurrent.{ScheduledFuture, ScheduledThreadPoolExecutor, TimeUnit}
 import scala.util.Random
 
 abstract class Ghosts(val MainColor: Color) extends Entity {
-  direction = Directions.Right
+  direction = Directions.Up
 
   protected var isVulnerable: Boolean = false;
   protected var isBlinking: Boolean = false;
-  protected var isAlive: Boolean = false;
 
   def IsVulnerable = isVulnerable;
   def IsBlinking = isBlinking;
-  def IsAlive = isAlive;
 
-  def kill: Unit = { isAlive = false; }
-  def revive: Unit = { isAlive = true; }
-
-  private val ex: ScheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+  private val makeBlinkThreadExecutor: ScheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+  private var currentMakeBlinkTask: Option[ScheduledFuture[_]] = None;
   private val makeBlinkTask = new Runnable {
     override def run(): Unit = {
-      if(IsVulnerable && !IsBlinking) {
-          isBlinking = true
-          return;
-      }
-      isVulnerable = false;
-      isBlinking = false;
-      if(!ex.isShutdown) ex.shutdown();
-      println("makeBlinkTaskEnd")
+      if(IsVulnerable)
+        isBlinking = true
+    }
+  }
+  private val resetVulnerableThreadExecutor: ScheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+  private var currentResetVulnerabilityTask: Option[ScheduledFuture[_]] = None;
+  private val resetVulnerabilityTask = new Runnable {
+    override def run(): Unit = {
+      resetVulnerability();
     }
   }
 
+
+
   def makeVulnerable(): Unit = {
     // TO DO : Dynamic calcul timing of vulnerability
+    if(isVulnerable) {
+      if(currentResetVulnerabilityTask.isDefined) currentResetVulnerabilityTask.get.cancel(true)
+      if(currentMakeBlinkTask.isDefined) currentMakeBlinkTask.get.cancel(true)
+    }
     isVulnerable = true;
-    ex.scheduleAtFixedRate(makeBlinkTask, 5, 5, TimeUnit.SECONDS)
+    isBlinking = false;
+    currentMakeBlinkTask = Some(makeBlinkThreadExecutor.schedule(makeBlinkTask, 5, TimeUnit.SECONDS))
+    currentResetVulnerabilityTask = Some(resetVulnerableThreadExecutor.schedule(resetVulnerabilityTask, 10, TimeUnit.SECONDS))
   }
 
   def resetVulnerability(): Unit = {
@@ -60,7 +65,7 @@ abstract class Ghosts(val MainColor: Color) extends Entity {
     if(isLastCaseDoor) return;
     if(!currentCase.isInstanceOf[RoadCase]) return;
     val currentRoad = currentCase.asInstanceOf[RoadCase];
-    if(!isLastADoor && !currentRoad.IsIntersection || currentRoad.isGhostsSpawn) return;
+    if(!isLastADoor && !currentRoad.IsIntersection && !currentRoad.isGhostsSpawn) return;
 
     var dir: Directions = Directions.Right
     var ny: Int = -1
@@ -74,8 +79,12 @@ abstract class Ghosts(val MainColor: Color) extends Entity {
       nx = x + deltaX;
     } while (
       !logical.IsPointInTheMap(nx, ny) ||
-        (lx == nx && ly == ny) ||
-        logical.Map(ny)(nx).CaseType != CaseType.Road
+      (lx == nx && ly == ny) ||
+      !(
+        logical.Map(ny)(nx).CaseType == CaseType.Road ||
+        logical.Map(ny)(nx).CaseType == CaseType.Door &&
+        currentRoad.isGhostsSpawn
+      )
     )
     direction = dir;
 
