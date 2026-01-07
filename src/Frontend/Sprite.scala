7153a4dc4b2a11807abcf1e47ce748a8cdcbeac6
helder <helder.ribeiro@hes-so.ch>
2026-01-07 21:30:46 +0100

    Finished Sprite management & Renderer added

--- src/Frontend/Sprite.scala
+++
@@ -1,7 +0,0 @@
-package Frontend
-
-import java.awt.Color
-
-class Sprite(val size: Int, var pixels: Array[Array[Color]]) {
-
-}
---
+++ src/Frontend/Sprite/SpriteType.scala
@@ -0,0 +1,6 @@
+package Frontend.Sprite
+
+object SpriteType extends Enumeration {
+  type SpriteType = Value;
+  val Road, Wall, Door, Empty, Ghost, Player = Value;
+}
